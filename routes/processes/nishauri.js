f1bd980f96ac670cfa75c6165485c6458f489dcf
vicwere <vwere@healthresearchkenya.org>
2023-05-09 13:14:52 +0300

    format cp_results

--- routes/processes/nishauri.js
+++ routes/processes/nishauri.js
@@ -1197,14 +1197,14 @@ router.get('/vl_results', async(req, res) =>  {
                       sp_status.push({result:'<LDL copies/ml', status: 'Viral Suppressed', date: lab_order_date_ })
                       //console.log(sp_status);
                   } else {
-                    if(value_<1000)
+                    if(value_.replace(/[^0-9]/g, '')<1000)
                     {
-                      sp_status.push({result:value_+' copies/ml', status: 'Viral UnSuppressed', date: lab_order_date_ })
+                      sp_status.push({result:value_.replace(/[^0-9]/g, '')+' copies/ml', status: 'Viral UnSuppressed', date: lab_order_date_ })
 
                      
                     }else
                     {
-                      sp_status.push({result:value_+' copies/ml', status: 'Viral Suppressed', date: lab_order_date_ })
+                      sp_status.push({result:value_.replace(/[^0-9]/g, '')+' copies/ml', status: 'Viral Suppressed', date: lab_order_date_ })
 
                     }
                    
