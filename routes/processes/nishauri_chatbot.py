64a85b375e61270d3c868c3f2858f6d8dcb53656
Joshua Murithi <brodiamyzalius@gmail.com>
2024-03-04 22:16:49 +0300

    (ops):Node nodules intergration
    Signed-off-by: Joshua Murithi <brodiamyzalius@gmail.com>

--- routes/processes/nishauri_chatbot.py
+++ routes/processes/nishauri_chatbot.py
@@ -10,7 +10,7 @@ question = 'How do i reschedule appointment?';
 # calcluate the total using variable values and print the output
 def chat_nishauri(question):
     from gradio_client import Client;
-    client = Client("http://192.168.1.44:7861", verbose=False);
+    client = Client("http://172.17.0.1:7861", verbose=False);
     result = client.predict(
 		question,	# str  in 'question' Textbox component
 		api_name="/predict"
